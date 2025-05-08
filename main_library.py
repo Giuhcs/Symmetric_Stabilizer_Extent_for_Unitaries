7f908613a5d1fbb12a803572275408dec4c7cb6d
Giulio Work <gcamillo@ugr.es>
2025-05-08 14:35:03 +0200

    adding target states to main library

---
+++ main_library.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+#### options of target state ####
+def tensor_T(n):
+    """ This function returns the tensor product of n T gates 
+    as a (complex) numpy 1d-array """
+    T=np.array([1.0,np.exp(1j*np.pi/4)])
+    aux=np.array(1.0)
+    for i in range(n):
+        aux=np.kron(aux,T)
+    return aux
+
+def U(theta,n):
+    """ This function returns the C^(n-1)-U(theta) on n qubits 
+    as a (complex) numpy 1d-array """
+    return np.array([1.0]*(2**n-1)+[np.exp(1j*theta)])
+
+def multi_CZ(n):
+    """ This function returns the C^(n-1)-Z gate on n qubits 
+    as a (complex) numpy 1d-array """
+    out = np.array([1.0]*(2**n-1)+[-1.0])
+    if n<=1:
+        out = 0
+    return out
+
+def multi_CS(n):
+    """ This function returns the C^(n-1)-S gate on n qubits 
+    as a (complex) numpy 1d-array """
+    out = np.array([1.0]*(2**n-1)+[1j])
+    if n<=1:
+        out = 0
+    return out
